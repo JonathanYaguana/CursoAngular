142ac28103dd1e847934679f74a0f1c273e80705
JonathanYaguana <jonathansacrox93@gmail.com>
2023-12-09 17:58:51 -0500

    funcionalidad Counter

--- 02-bases/src/app/counter/counter.component.ts
+++ 02-bases/src/app/counter/counter.component.ts
@@ -1,9 +1,28 @@
+import { AppComponent } from './../app.component';
 import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola Counter</h1>',
+  template: `
+  <h3>Contador: {{counter}}</h3>
+
+  <button (click)="increaseBy(+1)">+1</button>
+  <button (click)="reset()">Reset</button>
+  <button (click)="increaseBy(-1)">-1</button>
+
+  `
+
 })
+
 export class counterComponent{
 
+  public counter: number = 10;
+
+  increaseBy(value: number): void{
+    this.counter +=value;
+  }
+
+  reset():void{
+    this.counter = 10;
+  }
 }
