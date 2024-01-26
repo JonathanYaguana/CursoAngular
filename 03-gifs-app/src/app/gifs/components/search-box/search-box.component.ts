2ff4110677fb430f49de159adc2c478d78290c9d
JonathanYaguana <jonathansacrox93@gmail.com>
2024-01-26 14:41:51 -0500

    union de ramas

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 
 @Component({
@@ -13,18 +12,3 @@ import { Component } from '@angular/core';
 export class SearchBoxComponent {
   constructor (){ }
 }
-=======
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'gifs-search-box',
-  template: `
-    <h5>Buscar:</h5>
-    <input type="text" class="form-control" placeholder="Buscar gifs...">
-  `
-})
-
-export class SearchBoxComponent {
-  constructor() {}
-}
->>>>>>> b0631e29eab62c79d7771cf92ea4d6c48c2af3cc
--- 03-gifs-app/src/app/gifs/pages/home/home-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/home/home-page.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-<<<<<<< HEAD
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html'
 })
-=======
-  selector: 'app-home',
-  templateUrl: './home-page.component.html'
-})
-
->>>>>>> b0631e29eab62c79d7771cf92ea4d6c48c2af3cc
 export class HomePageComponent {
-
+  constructor (){}
 }
