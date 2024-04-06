8216963eb8b6ef4d29772a024265e3c7c7cc4d21
JonathanYaguana <jonathansacrox93@gmail.com>
2024-04-05 20:11:35 -0500

    caterror

--- 04-country-app/src/app/countries/services/countries.service.ts
+++ 04-country-app/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 import { Country } from '../interfaces/countries';
 
 @Injectable({providedIn: 'root'})
@@ -14,7 +14,10 @@ export class CountriesService {
 
     const url = `${this.apiUrl}/capital/${term}`;
 
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError( error => of([]) )
+    );
 
   }
 }
