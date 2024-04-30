dd90bdbd524f7196e2811a31c8d61b3bda047dd2
JonathanYaguana <jonathansacrox93@gmail.com>
2024-04-30 16:08:31 -0500

    Pipe i18nSelect

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PrimeNgModule } from '../../../prime-ng/prime-ng.module';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./uncommon-page.component.css']
 })
 export class UncommonPageComponent {
+
+  // i18n Select
+  public name: string = 'Jonathan'
+  public gener: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  chageClient(): void {
+    this.name = 'Jaina';
+    this.gener = 'female';
+  }
 
 }
