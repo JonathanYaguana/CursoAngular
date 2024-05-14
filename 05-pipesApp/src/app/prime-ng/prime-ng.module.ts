b6547b1c4326c68df11941d0d87d239ceb79f0d7
JonathanYaguana <jonathansacrox93@gmail.com>
2024-05-14 17:13:29 -0500

    valores y personalización pipes

--- 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 
+//import { MenuModule } from 'primeng/menu';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
-//import { MenuModule } from 'primeng/menu';
-import { PanelModule } from 'primeng/panel';
 import { MenubarModule } from 'primeng/menubar';
+import { PanelModule } from 'primeng/panel';
+import { ToolbarModule } from 'primeng/toolbar';
 
 
 @NgModule({
   exports:[
+    //MenuModule,
     ButtonModule,
     CardModule,
     FieldsetModule,
-    //MenuModule,
+    MenubarModule,
     PanelModule,
-    MenubarModule
+    ToolbarModule
   ]
 })
 
