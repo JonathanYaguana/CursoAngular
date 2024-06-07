561ef634b9067144c432b8640c89bcdb1b602f27
JonathanYaguana <jonathansacrox93@gmail.com>
2024-06-07 09:38:33 -0500

    traer y mostrar listado de héroes

--- 06-heroesApp/src/app/heroes/heroes.module.ts
+++ 06-heroesApp/src/app/heroes/heroes.module.ts
@@ -9,6 +9,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { CardComponent } from './components/card/card.component';
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
+    CardComponent,
   ],
   imports: [
     CommonModule,
