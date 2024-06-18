fb8cb33260385ebb1dcccd14752a18441bcf8420
JonathanYaguana <jonathansacrox93@gmail.com>
2024-06-18 16:39:00 -0500

    formulario new Page

--- 06-heroesApp/src/app/heroes/pages/search-page/search-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -8,5 +12,30 @@ import { Component } from '@angular/core';
 })
 export class SearchPageComponent {
 
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectHero?:  Hero;
+
+  constructor( private heroeService: HeroesService) {}
+
+  searchHero(){
+
+    const value: string = this.searchInput.value || '';
+
+    this.heroeService.getSuggestions( value )
+      .subscribe( heroes => this.heroes = heroes );
+  }
+
+  onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+    if( !event.option.value){
+      this.selectHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue( hero.superhero );
+
+    this.selectHero = hero;
+  }
 
 }
