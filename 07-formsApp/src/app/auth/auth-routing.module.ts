65fe4b4d7d71c68d529a9eea7d5bba870f0f3ec6
JonathanYaguana <jonathansacrox93@gmail.com>
2024-07-01 18:06:16 -0500

    rutas y barra de navegacion

--- 07-formsApp/src/app/auth/auth-routing.module.ts
+++ 07-formsApp/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterPAgeComponent } from './pages/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'sing-up', component: RegisterPAgeComponent },
+      { path: '**', redirectTo: 'sign-up' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
