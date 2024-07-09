7621dc2dbc8446d4fe85ed50dceec0c4f9d7917a
JonathanYaguana <jonathansacrox93@gmail.com>
2024-07-09 10:18:54 -0500

    mostrar errores de validación

--- 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 5000,
+  inStorage: 6
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
+
+  // public myForm: FormGroup = new FormGroup ({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   isStorage: new FormControl(0),
+  // });
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  })
+
+  constructor( private fb: FormBuilder ){}
+
+  ngOnInit(): void {
+    //this.myForm.reset( rtx5090 );
+  }
+
+  isValidField( field: string ){
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  onSave(): void{
+
+    if( this.myForm.invalid ) {
+
+      this.myForm.markAllAsTouched();
+
+      return;
+
+    }
+
+    this.myForm.reset();
+
+    console.log(this.myForm.value)
+  }
+
 
 }
