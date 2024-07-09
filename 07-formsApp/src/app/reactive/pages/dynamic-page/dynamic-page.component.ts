7f3ccefdb86c21b7dd421e1e7ea07ff229c8e8ae
JonathanYaguana <jonathansacrox93@gmail.com>
2024-07-09 12:01:17 -0500

    Formularios Dinamicos

--- 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    favoriteGame: this.fb.array([
+      ['WoW', Validators.required],
+      ['LoL', Validators.required]
+    ])
+  })
+
+  constructor ( private fb: FormBuilder) {}
+
+  onSubmit(): void {
+    if( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
 }
