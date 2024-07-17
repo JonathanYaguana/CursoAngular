a727ff887c97953ce77f1f07ecb3ae32cbc17310
JonathanYaguana <jonathansacrox93@gmail.com>
2024-07-17 12:31:41 -0500

    validar dos valores dentro del formulario

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -19,7 +19,10 @@ export class RegisterPageComponent {
     username: ['', [ Validators.required, this.validatorService.cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6)]],
     password2: ['', [ Validators.required ]],
-
+  }, {
+    validators: [
+      this.validatorService.isFieldOneEqualsFieldTwo('password','password2')
+    ]
   })
   constructor(
     private fb: FormBuilder,
--- 07-formsApp/src/app/shared/validators/email-validator.service.ts
+++ 07-formsApp/src/app/shared/validators/email-validator.service.ts
@@ -10,11 +10,36 @@ export class EmailValidatorService  implements AsyncValidator{
     const email = control.value;
     console.log(email);
 
-    return of({
-      emailTaken: true
+    const httpCallObservable = new Observable<ValidationErrors| null>( (subscriber) => {
+      console.log({ email });
+
+      if( email === 'jmyaguana@gmail.com' ){
+        subscriber.next({ emailTaken: true });
+        subscriber.complete();
+        //return;
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+
     }).pipe(
-      delay(2000)
-    )
+      delay( 3000 )
+    );
+
+    return httpCallObservable;
   }
+
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+
+  //   const email = control.value;
+  //   console.log(email);
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay(2000)
+  //   )
+  // }
 
 }
