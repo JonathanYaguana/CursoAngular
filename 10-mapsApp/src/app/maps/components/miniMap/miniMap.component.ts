1f51ab09216521e22759689acb246013b376268e
JonathanYaguana <jonathansacrox93@gmail.com>
2024-07-23 11:12:48 -0500

    rutas y menu de la app

---
+++ 10-mapsApp/src/app/maps/components/miniMap/miniMap.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-map',
+  templateUrl: './minimap.component.html',
+  styleUrls: ['./miniMap.component.css'],
+})
+export class MiniMapComponent { }
