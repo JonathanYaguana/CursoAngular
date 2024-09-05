6863cafebba04e7a6588062411cfa7ae5abaf419
JonathanYaguana <jonathansacrox93@gmail.com>
2024-09-05 16:22:31 -0500

    añadir coordenadas centrales del mapa

--- 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,17 +1,22 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 
-import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import { LngLat, Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css']
 })
-export class ZoomRangePageComponent implements AfterViewInit{
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy{
+
+  ngOnDestroy(): void {
+    this.map?.remove();//remover el mapa y todos los listener
+  }
 
   @ViewChild('map') divMap?: ElementRef;
 
   public zoom: number = 10;
   public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-79.20634578867782, -3.976688740069818);
 
   ngAfterViewInit(): void {
 
@@ -20,7 +25,7 @@ export class ZoomRangePageComponent implements AfterViewInit{
     this.map = new Map({
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
 
@@ -38,6 +43,12 @@ export class ZoomRangePageComponent implements AfterViewInit{
     this.map.on('zoomend', (ev) => {
       if ( this.map!.getZoom() < 18 ) return;
       this.map!.zoomTo(18)
+    })
+
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter();
+      const { lng, lat } = this.currentLngLat;
+      //console.log(this.currentLngLat);
     })
   }
 
