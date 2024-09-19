e74d2f50f60f40683be5f2ea2296eca2eb28f814
JonathanYaguana <jonathansacrox93@gmail.com>
2024-09-19 13:51:59 -0500

    standalone - componentes reutilizables

--- 10-mapsApp/src/app/alone/pages/alone-page/alone-page.component.ts
+++ 10-mapsApp/src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+//import { CommonModule } from '@angular/common';
+import { CounterAloneComponent } from "../../components/counter-alone/counter-alone.component";
 
 @Component({
-  selector: 'app-alone-page',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './alone-page.component.html',
-  styleUrls: ['./alone-page.component.css']
+  styleUrls: ['./alone-page.component.css'],
+  imports: [ CounterAloneComponent ]
 })
 export class AlonePageComponent {
 
