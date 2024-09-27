45ab37bc9d14e814e45e6cd0d2c1b730fee2ba00
JonathanYaguana <jonathansacrox93@gmail.com>
2024-09-27 18:35:07 -0500

    cambio de texto de la etiqueta

--- 11-directiveSignals/src/app/shared/directives/custom-label.directive.ts
+++ 11-directiveSignals/src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]',
@@ -7,6 +8,7 @@ export class CustomLabelDirective {
 
   private htmlElement?: ElementRef<HTMLElement>
   private _calor: string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input()
   set color (value: string){
@@ -14,17 +16,37 @@ export class CustomLabelDirective {
     this.setStyle();
   }
 
-  constructor( private el: ElementRef<HTMLElement> ) {
-    console.log(el)
-    this.htmlElement = el;
+  @Input()
+  set errors ( values: ValidationErrors | null | undefined){
+    this._errors = values;
+    this.setErrorMessage();
+  }
 
-    this.htmlElement.nativeElement.innerHTML = 'hola mundo';
+  constructor( private el: ElementRef<HTMLElement> ) {
+    //console.log(el)
+    this.htmlElement = el;
   }
 
   setStyle():void {
     if( !this.htmlElement ) return;
 
     this.htmlElement!.nativeElement.style.color = this._calor;
+  }
+
+  setErrorMessage():void {
+    if ( !this.htmlElement ) return;
+    if ( !this._errors ) {
+      this.htmlElement.nativeElement.innerText = 'No hay errores';
+      return;
+    }
+
+    const errors = Object.keys(this._errors)
+
+    if ( errors.includes('required')){
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
+
   }
 
 }
