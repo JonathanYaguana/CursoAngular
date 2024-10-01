c82b1d2914c7839861ee22f76c8cc2c2c0247089
JonathanYaguana <jonathansacrox93@gmail.com>
2024-10-01 17:57:24 -0500

    Counter signal

--- 11-directiveSignals/src/app/signals/pages/counterPage/counterPage.component.ts
+++ 11-directiveSignals/src/app/signals/pages/counterPage/counterPage.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './CounterPage.component.html' ,
   styleUrls: ['./counterPage.component.css'],
 })
-export class CounterPageComponent { }
+export class CounterPageComponent {
+
+  public counter = signal(10);
+  public squareCounter = computed( () => this.counter() * this.counter() );
+
+  incrementBy( value: number) {
+    this.counter.update( currrent => currrent + value );
+  }
+
+}
