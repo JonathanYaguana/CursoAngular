2914c183ec4a905255494116da422bc590d38a55
JonathanYaguana <jonathansacrox93@gmail.com>
2024-10-15 09:58:42 -0500

    estructura del proyecto

---
+++ 13-authApp/src/app/auth/layouts/auth-layout/auth-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-layout.component.html',
+  styleUrl: './auth-layout.component.css'
+})
+export class AuthLayoutComponent {
+
+}
---
+++ 13-authApp/src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+  templateUrl: './dashboard-layout.component.html',
+  styleUrl: './dashboard-layout.component.css'
+})
+export class DashboardLayoutComponent {
+
+}
