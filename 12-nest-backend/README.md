c9a025bd03433bed22dd23ef65851a2aef1cf278
JonathanYaguana <jonathansacrox93@gmail.com>
2024-10-15 02:48:08 -0500

    generar jwt

--- 12-nest-backend/README.md
+++ 12-nest-backend/README.md
@@ -4,3 +4,8 @@ docker compose up -d
 ```
 
 Copiar el ```.env.template``` y renombrar a ````.env```
+
+Instalación del jwt
+```
+npm install --save @nestjs/jwt
+```
--- 12-nest-backend/src/auth/auth.service.ts
+++ 12-nest-backend/src/auth/auth.service.ts
@@ -7,6 +7,9 @@ import { Model } from 'mongoose';
 
 import * as bcryptjs from 'bcryptjs';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+
+import { JwtPayload } from './entities/jwt-payload';
 
 @Injectable()
 export class AuthService {
@@ -14,6 +17,7 @@ export class AuthService {
   constructor( 
     @InjectModel( User.name )
     private userModel: Model<User>,
+    private jwtService: JwtService, 
   ){}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
@@ -60,7 +64,7 @@ export class AuthService {
 
     return {
       user: rest,
-      token: 'ABC-123'
+      token: this.getJwtToken({ id: user.id }),
     }
     /**
      * User { _id, name, email, roles, }
@@ -82,5 +86,10 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  getJwtToken( payload: JwtPayload ){
+    const token = this.jwtService.sign( payload );
+    return token;
   }
 }
