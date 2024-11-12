e0c360cfa0192e4a5323f21331026d9a2a4a28f6
JonathanYaguana <jonathansacrox93@gmail.com>
2024-11-11 21:01:38 -0500

    estructura del proyecto

---
+++ 14-my-dashborad/src/app/shared/heavy-loaders-slow/users-loaders.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'app-users-loaders',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<h1>Hola mundo</h1>`
+})
+export class UsersLoadersComponent {
+
+}
